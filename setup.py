30a8eb6141bea0b52afd2cf0665405dac679a8c6
Jeff Doozan <github@doozan.com>
2025-02-14 15:26:18 -0300

    added setup files for module installation

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='enwiktionary_wordlist',
+    description='Library for handling wordlist data extracted from en.wiktionary.org',
+    url='https://github.com/doozan/enwiktionary_wordlist',
+    author='Jeff Doozan',
+    author_email='github@doozan.com',
+    license='GPL 3',
+    packages=['enwiktionary_wordlist'],
+    scripts=[
+        'scripts/make_extract',
+        'scripts/make_wordlist',
+        'scripts/make_all_forms',
+        'scripts/wordlist_to_dictunformat',
+        'scripts/kaikki_to_wordlist',
+        ],
+    install_requires=[
+        'pywikibot',
+        'enwiktionary_sectionparser @ git+https://github.com/doozan/enwiktionary_sectionparser.git',
+        'enwiktionary_templates @ git+https://github.com/doozan/enwiktionary_templates.git',
+    ],
+)
