9297e5a25957a27ca6ee3e34d357800fdd1f70f9
Jeff Doozan <github@doozan.com>
2025-02-14 15:24:30 -0300

    added module data

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
+"""
+enwiktionary_wordlist
+
+Module for creating and manipulating data extracted from en.wiktionary.org
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Jeff Doozan'
+
 from . import (sense, word, wordlist)
